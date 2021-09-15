761f15b66af6eaf1252825a392fcf25c9ba178db
craigrmills <craig.r.mills86@gmail.com>
2021-09-15 10:15:05 -0500

    Added ShippingScreen and functionality

--- battlesmithfrontend/src/App.js
+++ battlesmithfrontend/src/App.js
@@ -7,6 +7,8 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
             <Route path='/' component={HomeScreen} exact />
             <Route path='/login' component={LoginScreen} />
             <Route path='/register' component={RegisterScreen} />
+            <Route path='/profile' component={ProfileScreen} />
+            <Route path='/shipping' component={ShippingScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
           </Container>
