ff91232261a64adcc66791ce8e0eb33212239724
craigrmills <craig.r.mills86@gmail.com>
2021-09-16 16:47:54 -0500

    Added Admin Delete Product Functionality

--- battlesmithfrontend/src/screens/UserListScreen.js
+++ battlesmithfrontend/src/screens/UserListScreen.js
@@ -25,7 +25,7 @@ function UserListScreen({history}) {
         }else {
             history.push('/login')
         }
-    }, [dispatch, history, successDelete])
+    }, [dispatch, history, successDelete, userInfo])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure you want to delete this user?')){
@@ -59,7 +59,7 @@ function UserListScreen({history}) {
                                     <i className='fas fa-check' style={{color: 'red'}}></i>
                                 )}</td>
                                 <td>
-                                    <LinkContainer to={`/admin/user/${user._id}`}>
+                                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
                                         <Button variant='light' className='btn-sm'>
                                             <i className='fas fa-edit'></i>
                                         </Button>
